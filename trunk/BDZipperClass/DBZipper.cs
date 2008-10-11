8eb9b58868630d0fd5b16510d75d122005f0b0fe
brettski <brettski@b614044e-9744-11dd-bbfe-3dabce05a288>
2008-10-11 04:09:20 +0000

    Initial load of BDZipper class. (take 3) Got to be the charm, right?

---
+++ trunk/BDZipperClass/DBZipper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BDZipper
+{
+    /// <summary>
+    /// Main class for our app
+    /// </summary>
+    public class BDZipper
+    {
+        public BDZipper() 
+        {
+        }
+        /// <summary>
+        /// Sub-directories of the current directory
+        /// </summary>
+        public List<DirectoryInfo> Directories
+        { get; set; }
+        /// <summary>
+        /// file in current directory
+        /// </summary>
+        public List<FileInfo> Files
+        { get; set; }
+
+        public string CurrentDirectory
+        { get; set; }
+
+
+
+    }
+
+
+}
