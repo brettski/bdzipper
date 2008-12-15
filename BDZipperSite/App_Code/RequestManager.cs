ed25d3c34123b809b0375c485eb020a092c5b129
brettski <brettski@b614044e-9744-11dd-bbfe-3dabce05a288>
2008-12-15 05:11:57 +0000

    Many class updates.  Have a pre-alpha publish which navigates files and directories.  Need security checks before navigating to directories. Need to get the breadcrumb running and maybe a start button to get back to starting directory when lost.  :)

--- BDZipperSite/App_Code/RequestManager.cs
+++ BDZipperSite/App_Code/RequestManager.cs
@@ -16,17 +16,29 @@ namespace BDZipper.Site
         private const string subDir = "sd";
 
         # endregion
-
+        /// <summary>
+        /// URL Param, Subdirectory (sd). Read Only.
+        /// </summary>
         public static string sd
         {
             get
             {
+                //return GetQueryStingParameter(subDir);
                 string rValue = HttpContext.Current.Request[subDir];
                 if (rValue != null)
                     return rValue;
                 else
                     return "";
             }
+        }
+
+        private static string GetQueryStingParameter(string param)
+        {
+            if (null != HttpContext.Current.Request[param])
+                return HttpContext.Current.Request[param];
+            else
+                return "";
+
         }
     }
 }
